0cff203b5f772a5c8dba999cbb47ff1a415340de
thesadru <dan0.suman@gmail.com>
2020-12-08 09:14:16 +0100

    Finished day 8.
    There's definitely an algorith for this, but I don't know it, so bruteforcing it is.
    Also the instruction change is awful.

---
+++ challenges/challenge-08-2.py
@@ -0,0 +1,28 @@
+with open('challenges/input-08.txt') as file:
+    raw = file.read()
+
+instructions = [[i[0],int(i[1])] for i in [i.split() for i in raw.splitlines()]]
+for i in range(len(instructions)):
+    instructions[i][0] = 'nop' if instructions[i][0]=='jmp' else ('jmp' if instructions[i][0]=='nop' else 'acc')
+    
+    hist = set()
+    accu = 0
+    pointer = 0
+    while pointer<len(instructions):
+        inst,val = instructions[pointer]
+        if inst=='acc':
+            accu += int(val)
+            pointer += 1
+        elif inst=='nop':
+            pointer += 1
+        elif inst=='jmp':
+            pointer += int(val)
+        
+        if pointer in hist:
+            break
+        hist.add(pointer)
+    else:
+        print(accu)
+        break
+    
+    instructions[i][0] = 'nop' if instructions[i][0]=='jmp' else ('jmp' if instructions[i][0]=='nop' else 'acc')
