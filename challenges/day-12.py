133862d4ae6a15c68502192fa0c60c855e75df15
thesadru <dan0.suman@gmail.com>
2020-12-12 11:39:31 +0100

    Edited day 12.
    Goddamn that looks so much better,

--- challenges/day-12.py
+++ challenges/day-12.py
@@ -1,4 +1,3 @@
-import math
 with open('inputs/input-12.txt') as file:
     raw = file.read()
 
@@ -9,31 +8,18 @@ def part1():
     x,y = 0,0 # a right,up direction (maths and shit)
     
     for c,a in data:
-        # a switch would be nice
-        if c in 'N':
-            y += a
-        elif c=='S':
-            y -= a
-        elif c=='E':
-            x += a
-        elif c=='W':
-            x -= a
+        if   c=='N': y += a
+        elif c=='S': y -= a
+        elif c=='E': x += a
+        elif c=='W': x -= a
         
-        elif c=='L':
-            rot -= a//90
-            rot %= 4
-        elif c=='R':
-            rot += a//90
-            rot %= 4
+        elif c=='L': rot -= a//90; rot %= 4
+        elif c=='R': rot += a//90; rot %= 4
         elif c=='F':
-            if rot==0:
-                y += a
-            elif rot==1:
-                x += a
-            elif rot==2:
-                y -= a
-            elif rot==3:
-                x -= a
+            if   rot==0: y += a
+            elif rot==1: x += a
+            elif rot==2: y -= a
+            elif rot==3: x -= a
     
     return abs(x)+abs(y)
 
@@ -41,37 +27,24 @@ def rotate(x,y,rot):
     """
     Rotates a point around 0,0.
     """
-    if rot == 0:
-        return  x, y
-    elif rot == 90:
-        return  y,-x
-    elif rot == 180:
-        return -x,-y
-    elif rot == 270:
-        return -y, x
+    if   rot==0:   return  x, y
+    elif rot==90:  return  y,-x
+    elif rot==180: return -x,-y
+    elif rot==270: return -y, x
 
 def part2():
     sx,sy = 0,0 # ship x and y
     wx,wy = 10,1 # relative waypoint x and y
     
     for c,a in data:
-        # a switch would be nice
-        if c=='N':
-            wy += a
-        elif c=='S':
-            wy -= a
-        elif c=='E':
-            wx += a
-        elif c=='W':
-            wx -= a
+        if   c=='N': wy += a
+        elif c=='S': wy -= a
+        elif c=='E': wx += a
+        elif c=='W': wx -= a
         
-        elif c=='L':
-            wx,wy = rotate(wx,wy,360-a)
-        elif c=='R':
-            wx,wy = rotate(wx,wy,a)
-        elif c=='F':
-            sx += wx*a
-            sy += wy*a
+        elif c=='L': wx,wy = rotate(wx,wy,360-a)
+        elif c=='R': wx,wy = rotate(wx,wy,a)
+        elif c=='F': sx += wx*a; sy += wy*a
     
     return abs(sx)+abs(sy)
 
