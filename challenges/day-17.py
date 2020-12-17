c3867acb90327b8224b42802ba0f8c828110c5f5
thesadru <dan0.suman@gmail.com>
2020-12-17 19:15:37 +0100

    Finished day 17.
    This was kinda fun, but a stupid bug got in and that wasn't fun.

---
+++ challenges/day-17.py
@@ -0,0 +1,36 @@
+from itertools import product
+with open('inputs/input-17.txt') as file:
+    raw = file.read().splitlines()
+
+def get_adjacent(origin: tuple[int], directions: list, table: set):
+    """
+    Counts how many cells are alive.
+    """
+    alive = 0
+    for d in directions:
+        pos = tuple(origin[i]+d[i] for i in range(len(origin))) # basically 1D matrix addition
+        alive += pos in table
+    return alive
+
+def conways_iteration(table: set, directions: list):
+    """
+    Gets all possible cells that can change, then add/remove/keep them.
+    """
+    old = table.copy() # save the old table for checking
+    for pos in product(*[range(min(i)-1,max(i)+2) for i in zip(*old)]): # iterate over all possible cells that can change
+        adjacent = get_adjacent(pos,directions,old)
+        if pos in old: # add/remove/keep logic
+            if adjacent not in (2,3): table.remove(pos)
+        else:
+            if adjacent == 3: table.add(pos)
+    return table
+
+def run(dimensions=3, iterations=6):
+    directions = [i for i in product((-1,0,1),repeat=dimensions) if any(i)] # generates all directions for neigbors
+    table = set((x,y)+(0,)*(dimensions-2) for x in range(len(raw[0])) for y in range(len(raw)) if raw[x][y]=='#') # generate a set of coordinates that are on
+    for _ in range(iterations):
+        table = conways_iteration(table,directions)
+    return len(table)
+
+print(run(3,6)) # 192 < 240 < x < 1504
+print(run(4,6)) # 
